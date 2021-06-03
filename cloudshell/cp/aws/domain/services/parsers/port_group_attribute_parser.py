f776e85abef25e0fe1f817225f8f52d4f65aa588
Kyrylo Maksymenko <saklar13@gmail.com>
2021-06-03 13:26:38 +0300

    Add possibility to pass icmp with no port number

--- cloudshell/cp/aws/domain/services/parsers/port_group_attribute_parser.py
+++ cloudshell/cp/aws/domain/services/parsers/port_group_attribute_parser.py
@@ -8,10 +8,16 @@ class PortGroupAttributeParser:
     PORT_DATA_MATCH = re.compile(
         r"^(?P<from_port>\d+)"
         r"(-(?P<to_port>\d+))?"
-        r"(:(?P<protocol>(udp|tcp|icmp)))?"
+        r"(:(?P<protocol>(udp|tcp)))?"
         r"(:(?P<destination>\S+))?$",
         re.IGNORECASE,
     )
+    ICMP_PORT_DATA_MATCH = re.compile(
+        r"^(?P<protocol>icmp)" r"(:(?P<destination>\S+))?$",
+        re.IGNORECASE,
+    )
+    DEFAULT_DESTINATION = "0.0.0.0/0"
+    DEFAULT_PROTOCOL = "tcp"
 
     @staticmethod
     def parse_security_group_rules_to_port_data(rules):
@@ -39,33 +45,35 @@ class PortGroupAttributeParser:
         :rtype: list[PortData]
         """
         if ports_attribute:
-            splitted_ports = filter(lambda x: x, ports_attribute.strip().split(";"))
+            ports = filter(bool, map(str.strip, ports_attribute.strip().split(";")))
             port_data_array = [
-                PortGroupAttributeParser._single_port_parse(port.strip())
-                for port in splitted_ports
+                PortGroupAttributeParser._single_port_parse(port) for port in ports
             ]
             return port_data_array
         return None
 
-    @staticmethod
-    def _single_port_parse(ports_attribute):
-        destination = "0.0.0.0/0"
-        protocol = "tcp"
+    @classmethod
+    def _single_port_parse(cls, ports_attribute):
+        match = cls.PORT_DATA_MATCH.search(ports_attribute)
+        if match:
+            from_port = match.group("from_port")
+            to_port = match.group("to_port") or from_port
+        else:
+            match = cls.ICMP_PORT_DATA_MATCH.search(ports_attribute)
+            if match:
+                from_port = to_port = "-1"
+            else:
+                msg = f"The value '{ports_attribute}' is not a valid ports rule"
+                raise ValueError(msg)
 
-        from_to_protocol_match = PortGroupAttributeParser.PORT_DATA_MATCH.search(
-            ports_attribute
+        destination = match.group("destination") or cls.DEFAULT_DESTINATION
+        protocol = match.group("protocol") or cls.DEFAULT_PROTOCOL
+        return PortData(
+            from_port=from_port,
+            to_port=to_port,
+            protocol=protocol,
+            destination=destination,
         )
-        if from_to_protocol_match:
-            port_data_params = from_to_protocol_match.groupdict()
-            if not port_data_params.get("to_port"):
-                port_data_params["to_port"] = port_data_params.get("from_port")
-            if not port_data_params.get("destination"):
-                port_data_params["destination"] = destination
-            if not port_data_params.get("protocol"):
-                port_data_params["protocol"] = protocol
-            return PortData(**port_data_params)
-
-        raise ValueError(f"The value '{ports_attribute}' is not a valid ports rule")
 
     @staticmethod
     def _is_valid_source(source):
