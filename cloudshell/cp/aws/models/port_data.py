e2f5aed03fc5713b9fee6c1c92f6753ca3eac44e
Kyrylo Maksymenko <saklar13@gmail.com>
2021-06-03 13:26:38 +0300

    Add possibility to pass icmp with no port number

--- cloudshell/cp/aws/models/port_data.py
+++ cloudshell/cp/aws/models/port_data.py
@@ -3,9 +3,9 @@ class PortData:
         """ec2_session  # noqa
 
         :param from_port: to_port-start port
-        :type from_port: int
+        :type from_port: str
         :param to_port: from_port-end port
-        :type to_port: int
+        :type to_port: str
         :param protocol: protocol-can be UDP or TCP
         :type protocol: str
         :param destination: Determines the traffic that can leave your instance, and where it can go.
