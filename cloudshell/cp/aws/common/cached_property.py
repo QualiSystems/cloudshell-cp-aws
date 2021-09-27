01056b538bf8402964912d48f3e649c6b86eee1b
Kyrylo Maksymenko <saklar13@gmail.com>
2021-09-27 19:37:17 +0300

    add cached_property

---
+++ cloudshell/cp/aws/common/cached_property.py
@@ -0,0 +1,14 @@
+class cached_property:
+    def __init__(self, func):
+        self.__doc__ = getattr(func, "__doc__")
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        value = obj.__dict__[self.func.__name__] = self.func(obj)
+        return value
+
+
+def invalidated_cache(instance, property_name: str):
+    del instance.__dict__[property_name]
